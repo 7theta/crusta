bd9bd79825372308c710a46f5b2e5d14f23148a7
Achint Sandhu <sandhu@7theta.com>
2017-12-09 08:45:38 -0500

    Upgrade to clojure 1.9

--- project.clj
+++ project.clj
@@ -8,16 +8,15 @@
 ;;   the terms of this license.
 ;;   You must not remove this notice, or any others, from this software.
 
-(defproject com.7theta/crusta "0.1.3"
+(defproject com.7theta/crusta "0.1.4"
   :description "A library for managing execution of external programs"
   :url "https://github.com/7theta/crusta"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.7theta/utilis "1.0.4"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.7theta/utilis "1.0.5"]]
   :profiles {:dev {:global-vars {*warn-on-reflection* true}
-                   :dependencies [[reloaded.repl "0.2.3"]
-                                  [org.clojure/tools.namespace "0.2.11"]]
+                   :dependencies [[org.clojure/tools.namespace "0.2.11"]]
                    :source-paths ["dev"]}}
   :clean-targets ^{:protect false} ["out" "target"]
   :scm {:name "git"
